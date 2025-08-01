d80b9ce814b8eab20e3d843b2f8cdbe3dd956485
yosuke08 <yousuke08@me.com>
2025-08-02 00:26:28 +0900

    fix: 画像出力の不具合を修正

--- examples/visualize_errors.py
+++ examples/visualize_errors.py
@@ -10,3 +10,4 @@ resistor.visualize_error_distribution(50)
 # 図をJPEG形式で保存
 plt.savefig('error_distribution.jpg', format='jpg', dpi=300, bbox_inches='tight')
 print("誤差分布の図が 'error_distribution.jpg' として保存されました。")
+
